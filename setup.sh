d365998a355391b47ab8b23285a593040f9e2796
Fabian <f.fritzer@gmail.com>
2018-06-11 14:29:33 +0200

    added reminders (TODO)

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #! /usr/bin/env bash
 
+#
+# Reminders:
+#  xfce-terminal: colors, history, disable f10
+#
+
 sudo <<EOF
 	# wireshark
 	apt-get install wireshark
 	usermod -a -G wireshark folfy
 
 	# utils
-	apt-get install curl stow
+	apt-get install curl stow tig ruby
+	# tmuxinator requires ruby
+	gem install tmuxinator
 EOF
