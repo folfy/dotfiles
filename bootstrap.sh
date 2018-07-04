df8a527164d5ee2607afd47a32658cef27b89a95
ffritzer <fabian.fritzer@thalesgroup.com>
2018-07-04 15:43:18 +0200

    force stow to keep .vim unfolded

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,11 @@
 dirs="$(ls -d */)"
 target="$HOME"
 
+if ! [ -d ~/.vim ]; then
+	# create as dir, avoid symlinking by stow
+	mkdir ~/.vim
+fi
+
 # verbose dry-run
 echo "Dry-run for setup to \"$target\" of dirs: "$dirs
 stow -vn -t "$target" $dirs
