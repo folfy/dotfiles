fc39a21b3585406054549a74f0a1ac5ce4e7fb28
Folfy <folfy@users.noreply.github.com>
2025-05-07 16:03:38 +0200

    tmux: fixed stats not working

--- tmux/bin/.tmuxstatus.sh
+++ tmux/bin/.tmuxstatus.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/env bash
 
-LC_NUMERIC=C
-(free -m; uptime) | awk -v host="$HOSTNAME" '
+(free -m; echo -ne "load average "; cat /proc/loadavg) | awk -v host="$HOSTNAME" '
 /^Mem:/ {
-total = $2
-used = $3
+	total = $2
+	used = $3
 }
 /^-\/+/ {
-used = $3
+	# used = $3
 }
 /load average/ {
-split($0, array, "[,:]")
-#users = gsub("[^0-9]", "", array[6])
-avg1 = array[8]
-avg2 = array[9]
-agv3 = array[10]
+	split($0, array, " ")
+	avg1 = array[3]
+	avg2 = array[4]
+	agv3 = array[5]
 }
 END {
-#printf("%su - %.1f / %.0fG, %s, %s", users, used/1000, total/1000, avg1, avg2)
-printf("%.1f / %.0fG, %.1f, %.1f", used/1000, total/1000, avg1, avg2)
+	# printf("%su - %.1f / %.0fG, %s, %s", users, used/1000, total/1000, avg1, avg2)
+	printf("%.1f / %.0fG, %.1f, %.1f", used/1000, total/1000, avg1, avg2)
 }
-' 2>/dev/null 
+' 2>/dev/null
